ff225e097d835b4d9858bbdcf180d44917f41cc6
Clément Murzeau <clement@studiorelief.fr>
2024-06-05 15:53:39 +0200

    update css

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,31 @@
-"use strict";(()=>{function c(){let o=(n,e)=>{n.forEach(r=>{r.addEventListener("click",()=>{document.querySelectorAll(`.${e}`).forEach(u=>{u.classList.remove("is-active")});let t=r.querySelector(`.${e}`);t&&t.classList.add("is-active")})})};o(document.querySelectorAll(".hero_number"),"hero_number-background"),o(document.querySelectorAll(".hero-canvas_c-button"),"hero_number-background")}window.Webflow||(window.Webflow=[]);window.Webflow.push(()=>{c()});})();
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/utils/tricks.ts
+  function buttonActive() {
+    const handleClick = (elements, targetClass) => {
+      elements.forEach((element) => {
+        element.addEventListener("click", () => {
+          document.querySelectorAll(`.${targetClass}`).forEach((bg) => {
+            bg.classList.remove("is-active");
+          });
+          const backgroundButton = element.querySelector(`.${targetClass}`);
+          if (backgroundButton) {
+            backgroundButton.classList.add("is-active");
+          }
+        });
+      });
+    };
+    handleClick(document.querySelectorAll(".hero_number"), "hero_number-background");
+    handleClick(document.querySelectorAll(".hero-canvas_c-button"), "hero_number-background");
+  }
+
+  // src/index.ts
+  window.Webflow ||= [];
+  window.Webflow.push(() => {
+    buttonActive();
+  });
+})();
+//# sourceMappingURL=index.js.map
